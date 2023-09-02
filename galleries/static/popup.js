01b5b77de08fa0a57482fb11ca2bbf98891857d6
Andrei Rotari <andreichina@gmail.com>
2023-09-02 03:48:19 -0700

    Fix bug: full image popup opens when moving image
    This fixes the bug that caused the full-size image popup to open after
    clicking on the buttons used to change the image's position in the
    gallery

--- galleries/static/popup.js
+++ galleries/static/popup.js
@@ -2,21 +2,16 @@
 let photoPopups = document.getElementsByClassName('photo-popup');
 for (let i = 0; i < photoPopups.length; i++) {
     photoPopups[i].addEventListener('click', function() {
-        this.classList.remove('photo-popup--visible');
-        // let bodyElement = document.getElementsByClassName('page__body');
-        // bodyElement.classList.remove('noscroll');
         document.body.classList.remove('noscroll');
+        this.classList.remove('photo-popup--visible');
     });
 }
 
 // add click event listener to the image to trigger the popup
-let galleryPhotos = document.getElementsByClassName('gallery__photo');
+let galleryPhotos = document.getElementsByClassName('gallery__img');
 for (let i = 0; i < galleryPhotos.length; i++) {
     galleryPhotos[i].addEventListener('click', function () {
-        // bodyElement.classList.add('noscroll');
-        this.nextElementSibling.classList.add('photo-popup--visible');
-        // let bodyElement = document.getElementsByClassName('page__body');
-        // bodyElement.classList.add('noscroll');
         document.body.classList.add('noscroll');
+        this.parentNode.nextElementSibling.classList.add('photo-popup--visible');
     });
 }
