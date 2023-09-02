406746f3265b6cd6140ecadb497ed09bdbac0337
Andrei Rotari <andreichina@gmail.com>
2023-09-02 03:42:36 -0700

    Fix CSS columns - IntersectionObserver bug
    This compensates for the bug that causes some images to fail to
    lazy-load. The bug is caused by difficulties in using CSS 'columns'
    layout with InterSection observer. After the page is loaded in addition
    to running the IntersectionObserver with lazy-loading on all images,
    the script waits and then checks for any cases where lazy-loading failed
    within the viewport, and forces lazy-loading of any such images.

--- galleries/static/lazy-observer.js
+++ galleries/static/lazy-observer.js
@@ -1,36 +1,71 @@
-const pictures = window.document.querySelectorAll('.gallery__photo');
+let imgObserver;
 
 const config = {
-    rootMargin: '20px 0px',
+    rootMargin: '200px 0px 200px 0px',
     threshold: 0.1
 };
 
-let imgObserver;
+initIntersectionObserver();
+
+setTimeout(() => {
+    forceLoad();
+}, 300);
+
+function isInViewport(photo) {
+    const rect = photo.getBoundingClientRect();
+    return (
+        rect.top < (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.bottom > 0 &&
+        rect.left < (window.innerWidth || document.documentElement.clientWidth) &&
+        rect.right > 0
+    );
+}
+
+function forceLoad() {
+    const photos = window.document.querySelectorAll('.gallery__photo');
+    photos.forEach((photo) => {
+        if (!photo.classList.contains('is-loaded') && isInViewport(photo)) {
+            lazyLoad(photo);
+        }
+    });
+}
+
+function initIntersectionObserver() {
+    imgObserver = new IntersectionObserver((entries, self) => {
+        entries.forEach(entry => {
+            if (entry.intersectionRatio > 0) {
+                lazyLoad(entry.target);
+                self.unobserve(entry.target);
+            }
+        });
+    }, config);
+
+    observeImages();
+}
+
+function observeImages() {
+    const pictures = window.document.querySelectorAll('.gallery__photo');
+
+    pictures.forEach((picture) => {
+        if (!picture.classList.contains('is-loaded')) {
+            imgObserver.observe(picture);
+        }
+    });
+}
+
 
 // replace the placeholder with actual image links
-let lazyLoad = (picture) => {
+function lazyLoad(picture) {
     const img = picture.querySelector('img');
     const sources = picture.querySelectorAll('source');
 
     sources.forEach((source) => {
         source.srcset = source.dataset.srcset;
-        source.removeAttribute('data-srcset');
+        // source.removeAttribute('data-srcset');
     });
     img.src = img.dataset.src;
-    img.removeAttribute('data-src');
+    // img.removeAttribute('data-src');
+
+    // Mark the image as loaded
+    picture.classList.add('is-loaded');
 }
-
-// initialize the intersection observer
-imgObserver = new IntersectionObserver((entries, self) => {
-    entries.forEach(entry => {
-        if (entry.intersectionRatio > 0) {
-            lazyLoad(entry.target);
-            self.unobserve(entry.target);
-        }
-    });
-}, config);
-
-// make the observer start observing the picture elements
-pictures.forEach((picture) => {
-    imgObserver.observe(picture);
-});
